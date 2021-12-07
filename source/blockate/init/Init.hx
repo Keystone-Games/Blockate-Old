07f88dd0dc3248b6ddf560fc663d2d1edfb3429d
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-07 10:00:11 -0500

    Update Init.hx

--- source/blockate/init/Init.hx
+++ source/blockate/init/Init.hx
@@ -1,3 +1,13 @@
 package blockate.init;
 
-class Init {}
+class Init {
+  public static function preInit(PreInitialisationArgs:Array<Dynamic>) {
+    init(PreInitialisationArgs);
+  }
+  
+  public static function init(InitialisationArgs:Array<Dynamic>) {
+    postInit(InitialisationArgs);
+  }
+  
+  public static function postInit(PostInitialisationArgs:Array<Dynamic>) {}
+}
