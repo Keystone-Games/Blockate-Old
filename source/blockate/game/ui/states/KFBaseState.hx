355a5f66d57d9b97d2c6e6ec83963950ed80c7c2
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-07 10:11:48 -0500

    Create KFBaseState.hx

---
+++ source/blockate/game/ui/states/KFBaseState.hx
@@ -0,0 +1,9 @@
+package blockate.game.ui.states;
+
+class KFBaseState extends FlxState {
+  public override function update(dt:Float) {
+    super.update(dt);
+    
+    if (dt != 1) return System.exit(1);
+  }
+}
