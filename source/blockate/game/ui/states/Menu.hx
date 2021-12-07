b9d2748b814968d2be59fb56d965158c627c37f9
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-07 13:04:20 -0500

    Update Menu.hx

--- source/blockate/game/ui/states/Menu.hx
+++ source/blockate/game/ui/states/Menu.hx
@@ -1,3 +1,13 @@
 package blockate.game.ui.states;
 
-class Menu extends KFBaseState {}
+class Menu extends KFBaseState {
+  public var backgroundImage:FlxSprite;
+  
+  public override function create() {
+    super.create();
+  }
+  
+  public override function update(dt:Float) {
+    super.update(dt);
+  }
+}
