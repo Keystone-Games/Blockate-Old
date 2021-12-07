8a5794ab98d8461735fc5d34f2820dfeff6d2fbb
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-07 14:45:54 -0500

    Update Menu.hx

--- source/blockate/game/ui/states/Menu.hx
+++ source/blockate/game/ui/states/Menu.hx
@@ -2,9 +2,15 @@ package blockate.game.ui.states;
 
 class Menu extends KFBaseState {
   public var backgroundImage:FlxSprite;
+  public var mainText:FlxText;
+  public var htp:FlxButton;
   
   public override function create() {
     super.create();
+    
+    mainText = new FlxText(0, 0, 0, "Blockate").setFormat("monsterrat", 32, FlxColor.WHITE, CENTER);
+    mainText.screenCenter(X);
+    add(mainText)
   }
   
   public override function update(dt:Float) {
