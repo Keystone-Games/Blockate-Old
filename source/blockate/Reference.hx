127f137f7bde97dfcce2e7deeb817cfb003a0446
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-07 10:05:46 -0500

    Update Reference.hx

--- source/blockate/Reference.hx
+++ source/blockate/Reference.hx
@@ -1,8 +1,12 @@
 package blockate;
 
+typedef InitialState = null;
+
 class Reference
 {
 	public static final APP_NAME:String = "Blockate";
 	public static final APP_ID:String = "blockate";
 	public static final APP_VERSION:String = "1.0.0";
+	
+	public static final CFR:Int = 1000;
 }
