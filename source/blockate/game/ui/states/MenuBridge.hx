bd26e0e09ff3f5cc9217280c4f7138bce8e35847
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-09 10:09:27 -0500

    Update MenuBridge.hx

--- source/blockate/game/ui/states/MenuBridge.hx
+++ source/blockate/game/ui/states/MenuBridge.hx
@@ -7,9 +7,18 @@ class MenuBridge extends KFBaseState {
   
   public override function create() {
     super.create();
+    
+    worldListButton = new FlxButton(0, 0, "My worlds", function() {worldListButtonClick();});
+    worldListButton.screenCenter(X);
+    worldListButton.scale.set(2,2);
+    add(worldListButton);
   }
   
   public override function update(dt:Float) {
     super.update(dt);
+  }
+  
+  private static function worldListButtonClick() {
+    FlxG.switchState(new WorldList());
   }
 }
