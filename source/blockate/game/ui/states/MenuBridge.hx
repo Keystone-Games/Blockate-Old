532486a519a83cd8030b089cae7f35ec7468cf0e
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-09 08:14:09 -0500

    Create MenuBridge.hx

---
+++ source/blockate/game/ui/states/MenuBridge.hx
@@ -0,0 +1,15 @@
+package blockate.game.ui.states;
+
+class MenuBridge extends KFBaseState {
+  public var worldListButton:FlxButton;
+  public var optionsMenuButton:FlxButton;
+  public var exitButton:FlxButton;
+  
+  public override function create() {
+    super.create();
+  }
+  
+  public override function update(dt:Float) {
+    super.update(dt);
+  }
+}
