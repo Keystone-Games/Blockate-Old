dd9f8ba287fb54261a9c2945d72b1301d4ff206e
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-01-07 15:00:32 -0500

    Create WorldList.hx

---
+++ source/blockate/game/ui/states/WorldList.hx
@@ -0,0 +1,11 @@
+package blockate.game.ui.states;
+
+class WorldList extends FlxState {
+  public override function create() {
+    super.create();
+  }
+  
+  public override function update(dt:Float) {
+    super.update(dt);
+  }
+}
