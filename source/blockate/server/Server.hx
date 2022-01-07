5623b2a53fc3dedcfc22f92749e8896bf06d6c55
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-01-07 14:58:39 -0500

    Update Server.hx

--- source/blockate/server/Server.hx
+++ source/blockate/server/Server.hx
@@ -1,1 +1,21 @@
-ed
+package blockate.server;
+
+class Server {
+  public static function ping(ip:String, port:Int) {
+    if (port > 65535 || port < 1025) { return {error: "port is invalid!"}; }
+    
+    var http = new haxe.Http("http://" + ip + ":" + port);
+
+    http.onData = function (data:String) {
+      var result = haxe.Json.parse(data);
+      trace(${result});
+      return result;
+    }
+        
+    http.onError = function (error) {
+      trace('error: $error');
+    }
+        
+    http.request();
+  }
+}
