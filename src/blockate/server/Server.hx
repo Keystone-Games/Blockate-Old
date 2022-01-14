23826c6e4a9614203b8e07efc6f7a49abb08d84a
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-01-14 14:26:21 -0500

    Update Server.hx

--- src/blockate/server/Server.hx
+++ src/blockate/server/Server.hx
@@ -1,22 +1,31 @@
 package blockate.server;
+
 import haxe.Http;
+import kglog.Logger;
 
-class Server {
-  public static function ping(ip:String, port:Int) {
-    if (port > 65535 || port < 1025) { return {error: "port is invalid!"}; }
-    
-    var http = new Http("http://" + ip + ":" + port);
+class Server
+{
+	public static function ping(ip:String, port:Int)
+	{
+		if (port > 65535 || port < 1025)
+		{
+			return {error: "port is invalid!"};
+		}
 
-    http.onData = function (data:String) {
-      var result = haxe.Json.parse(data);
-      Logger.simpleLog(${result});
-      return result;
-    }
-        
-    http.onError = function (error) {
-      trace('error: $error');
-    }
-        
-    http.request();
-  }
+		var http = new Http("http://" + ip + ":" + port);
+
+		http.onData = function(data:String)
+		{
+			var result = haxe.Json.parse(data);
+			Logger.simpleLog(${result});
+			return result;
+		}
+
+		http.onError = function(error)
+		{
+			trace('error: $error');
+		}
+
+		http.request();
+	}
 }
