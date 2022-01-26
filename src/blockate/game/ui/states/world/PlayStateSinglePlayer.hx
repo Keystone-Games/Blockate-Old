9dbba9ae8787b47fc111ec3b72b1acf7f6864729
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-01-26 18:32:34 -0500

    Update PlayStateSinglePlayer.hx

--- src/blockate/game/ui/states/world/PlayStateSinglePlayer.hx
+++ src/blockate/game/ui/states/world/PlayStateSinglePlayer.hx
@@ -1,6 +1,7 @@
 package blockate.game.ui.states.world;
 
 import blockate.game.util.PlayState;
+import kglog.Logger;
 
 /**
  * ...
@@ -13,8 +14,17 @@ class PlayStateSinglePlayer extends PlayState
 	public function new(worldFile:String) 
 	{
 		super(worldFile);
+		Logger.simpleLog("Loading " + worldFile);
 		
+		Logger.simpleLog("Defining state type");
 		stateType = SINGLEPLAYER;
+	}
+	
+	public override function create() {
+		super.create();
+	}
+	public override function update(dt:Float) {
+		super.update(dt);
 	}
 	
 }
