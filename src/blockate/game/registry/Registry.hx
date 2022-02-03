b4eca55060a9b171caa211bc7ebd2a11b25c1e83
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-02-03 08:11:16 -0500

    Update Registry.hx

--- src/blockate/game/registry/Registry.hx
+++ src/blockate/game/registry/Registry.hx
@@ -1,6 +1,23 @@
 package blockate.game.registry;
 
-class Registry {
-  public static final tile = Tile.list;
-  public static final item = Item.list;
+class Registry
+{
+	public static var items = {};
+
+	public static final ITEM:String = "item";
+	public static final TILE:String = "tile";
+
+	public static function register(args:{number:Int,meta:Int,id:Identifier,defenition:RegistryBase,type:String})
+	{
+		switch (args.type)
+		{
+			case "item":
+				items[Std.string(args.number) + ":" + Std.string(args.meta)] =
+				{
+					id: args.id,
+					item: args.defenition
+				};
+				break;
+		}
+	}
 }
