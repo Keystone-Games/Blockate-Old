7a6c106f95ff5b6bd50a78360baaddce675b7e7e
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-02-03 10:00:24 -0500

    Update Item.hx

--- src/blockate/game/registry/item/Item.hx
+++ src/blockate/game/registry/item/Item.hx
@@ -1,6 +1,13 @@
 package blockate.game.registry.item;
 import blockate.game.registry.RegistryBase;
+import blockate.game.util.ItemType;
 
 interface Item extends RegistryBase {
-  
+  /*
+	 * The texture of the tile.
+	 * type: FlxGraphicAsset
+	 */
+	public var texture:String;
+
+	public var type:ItemType;
 }
